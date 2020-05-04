c330fbd34161c4437bfc8e371ebd92c271c88fcf
Mauricio Ramirez <ramirezmauricio@hotmail.com>
2020-05-03 20:34:30 -0500

    Parallel Test Execution

---
+++ automated-web-testing/advanced_serenity-bdd/parallel-testing/src/test/java/serenitydojo/cucumber/parallel/search/SearchForm.java
@@ -0,0 +1,8 @@
+package serenitydojo.cucumber.parallel.search;
+
+import org.openqa.selenium.By;
+
+public class SearchForm {
+    public static By SEARCH_QUERY = By.id("query");
+    public static By SEARCH_BUTTON = By.id("queryButton");
+}
